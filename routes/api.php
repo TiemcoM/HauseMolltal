bcb03d985932633c56a7d8bb05695be312d9054b
Simon van den Berg <simonschrotenberg@gmail.com>
2021-04-10 18:09:08 +0200

    added update and change password for user

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,8 +24,8 @@ Route::group([
     'middleware' => 'auth:api'
 ], function () {
     Route::prefix('user')->group(function () {
-        Route::get('get', function (Request $request) {
-            return $request->user();
-        });
+        Route::get('get', [UserController::class, 'get']);
+        Route::put('update', [UserController::class, 'update']);
+        Route::put('changePassword', [UserController::class, 'changePassword']);
     });
 });
