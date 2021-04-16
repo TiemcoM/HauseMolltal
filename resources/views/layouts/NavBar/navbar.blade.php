1847dc1f778b83ebd57fd78b023af7992e488eb3
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-04-16 10:35:36 +0200

    updated header
    not working yet

--- resources/views/layouts/NavBar/navbar.blade.php
+++ resources/views/layouts/NavBar/navbar.blade.php
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-                @include('layouts.navbar.menuItems')
+                @include('layouts.navbar.navItems')
             </ul>
 
             <!-- Right Side Of Navbar -->
