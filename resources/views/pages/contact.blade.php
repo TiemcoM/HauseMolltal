b45a08e5152ecb22fe2e5ae08cf0b243425ee628
Xander-Greven <xandergerjangreven@gmail.com>
2021-04-23 11:35:18 +0200

    extra card voor contact info en styling

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,95 +6,113 @@
 
 @section('content')
 <br/>
-<!-- Success message -->
+
 @if(Session::has('success'))
-    <div class="alert alert-success">
-        {{Session::get('success')}}
-    </div>
-@endif
-<!-- captcha message -->
-@if(Session::has('captcha'))
-    <div class="alert alert-danger">
-        {{Session::get('captcha')}}
-    </div>
-@endif
-
-
-<script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}"></script>
-<script>
-         grecaptcha.ready(function() {
-             grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'contact'}).then(function(token) {
-                if (token) {
-                  document.getElementById('recaptcha').value = token;
-                }
-             });
-         });
-</script>
-
-<form action="" method="post" action="{{ route('contact.store') }}">
-
-    @csrf
-
-    <input type="hidden" name="recaptcha" id="recaptcha">
-
-    <div class="form-group">
-        <label>Naam</label>
-        <input type="text" class="form-control {{ $errors->has('name') ? 'error' : '' }}" name="name" id="name">
-
-        <!-- Error -->
-        @if ($errors->has('name'))
-        <div class="error">
-            {{ $errors->first('name') }}
-        </div>
+            <div class="alert alert-success">
+                {{Session::get('success')}}
+            </div>
         @endif
-    </div>
-
-    <div class="form-group">
-        <label>Email</label>
-        <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" name="email" id="email">
-
-        @if ($errors->has('email'))
-        <div class="error">
-            {{ $errors->first('email') }}
-        </div>
+        <!-- captcha message -->
+        @if(Session::has('captcha'))
+            <div class="alert alert-danger">
+                {{Session::get('captcha')}}
+            </div>
         @endif
+<div class="container-fluid">
+
+<div class="row">
+    <div class="col-md-7 p-0">
+        <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}"></script>
+        <script>
+                grecaptcha.ready(function() {
+                    grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'contact'}).then(function(token) {
+                        if (token) {
+                        document.getElementById('recaptcha').value = token;
+                        }
+                    });
+                });
+        </script>
+
+        <form action="" method="post" action="{{ route('contact.store') }}">
+
+            @csrf
+
+            <input type="hidden" name="recaptcha" id="recaptcha">
+
+            <div class="form-group">
+                <label>Naam</label>
+                <input type="text" class="form-control {{ $errors->has('name') ? 'error' : '' }}" name="name" id="name">
+
+                <!-- Error -->
+                @if ($errors->has('name'))
+                <div class="error">
+                    {{ $errors->first('name') }}
+                </div>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" name="email" id="email">
+
+                @if ($errors->has('email'))
+                <div class="error">
+                    {{ $errors->first('email') }}
+                </div>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label>Telefoon nummer</label>
+                <input type="text" class="form-control {{ $errors->has('phone') ? 'error' : '' }}" name="phone" id="phone">
+
+                @if ($errors->has('phone'))
+                <div class="error">
+                    {{ $errors->first('phone') }}
+                </div>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label>Onderwerp</label>
+                <input type="text" class="form-control {{ $errors->has('subject') ? 'error' : '' }}" name="subject"
+                    id="subject">
+
+                @if ($errors->has('subject'))
+                <div class="error">
+                    {{ $errors->first('subject') }}
+                </div>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label>Inhoud</label>
+                <textarea class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message" id="message"
+                    rows="4"></textarea>
+
+                @if ($errors->has('message'))
+                <div class="error">
+                    {{ $errors->first('message') }}
+                </div>
+                @endif
+            </div>
+
+            <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
+        </form>
+        <br />
+        <!-- Success message -->
+
+        <br />
     </div>
 
-    <div class="form-group">
-        <label>Telefoon nummer</label>
-        <input type="text" class="form-control {{ $errors->has('phone') ? 'error' : '' }}" name="phone" id="phone">
-
-        @if ($errors->has('phone'))
-        <div class="error">
-            {{ $errors->first('phone') }}
+    <div class="col-md-4 ml-auto p-0">
+        <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Contact gegevens</h5>
+            <p class="card-text">Hier komen de contact gegevens.</p>
         </div>
-        @endif
-    </div>
-
-    <div class="form-group">
-        <label>Onderwerp</label>
-        <input type="text" class="form-control {{ $errors->has('subject') ? 'error' : '' }}" name="subject"
-            id="subject">
-
-        @if ($errors->has('subject'))
-        <div class="error">
-            {{ $errors->first('subject') }}
         </div>
-        @endif
     </div>
-
-    <div class="form-group">
-        <label>Inhoud</label>
-        <textarea class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message" id="message"
-            rows="4"></textarea>
-
-        @if ($errors->has('message'))
-        <div class="error">
-            {{ $errors->first('message') }}
-        </div>
-        @endif
-    </div>
-
-    <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
-</form>
+</div>
+</div>
 @endsection
