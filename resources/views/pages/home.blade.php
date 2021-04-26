0ec7bf74faab2e137aef8ed9247517d3e7186e3e
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-04-26 16:19:12 +0200

    homepage good
    very nice

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,11 +5,14 @@ Home
 @extends('layouts.app')
 
 @section('content')
-    <div class="p-5 text-center bg-image"
+    <div class="p-5 text-center bg-image img-responsive"
          style="
-      background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.jpg');
+      background-image: url('http://www.hause-molltal.nl/foto%20huis%201.JPG');
       height: 400px;
       margin-top: 58px;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center center;
     ">
         <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
             <div class="d-flex justify-content-center align-items-center h-100">
@@ -18,6 +21,14 @@ Home
                     <h4 class="mb-3">Mooi huissie</h4>
                     <a class="btn btn-outline-light btn-lg" href="{{ route('gallery') }}" role="button">Meer afbeeldingen</a>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <div>
+
             </div>
         </div>
     </div>
