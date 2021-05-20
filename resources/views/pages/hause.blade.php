cdf709ef62e5d642b3717e189df9807b286f6438
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-05-20 16:16:46 +0200

    Hause page created

---
+++ resources/views/pages/hause.blade.php
@@ -0,0 +1,10 @@
+@section('title')
+    Het Huis
+@endsection
+
+@extends('layouts.app')
+
+@section('content')
+        <img src="{{ asset('assets/img/HuisOmgeving/huis/Hause.jpg') }}" alt="HauseMolltal" style="width:100%;">
+        <h2 class="text-center"><b>Het huis</b></h2>
+@endsection
