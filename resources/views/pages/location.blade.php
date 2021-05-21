33949baa939fd430fcb3d8de1f7bd375cb7d0e6f
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-05-21 09:57:56 +0200

    Added maps embed in the location page

--- resources/views/pages/location.blade.php
+++ resources/views/pages/location.blade.php
@@ -5,5 +5,8 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="mapouter">
+        <div class="gmap_canvas">
+            <iframe  width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Untersagritz%2012,%209843%20Untersagritz,%20Oostenrijk&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
     @endsection
