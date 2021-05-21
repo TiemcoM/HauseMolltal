f84cd0087104ca5947460c6a0dc42035631a7b5d
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-05-21 11:19:33 +0200

    Updated styling

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,49 +1,52 @@
 @section('title')
-Home
+    Home
 @endsection
 
 @extends('layouts.app')
 
 @section('content')
-<div class="bd-example">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{ asset('assets/img/HuisOmgeving/huis/P1030790.JPG') }}" class="d-block w-100" alt="HauseMoltall">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Hause Moltall</h5>
-                    <p>Kom tot rust in de Oostenrijkse natuur!</p>
+    <div class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('assets/img/HuisOmgeving/huis/P1030790.JPG') }}" class="d-block w-100"
+                         alt="HauseMoltall">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Hause Moltall</h5>
+                        <p>Kom tot rust in de Oostenrijkse natuur!</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/img/HuisOmgeving/zomer/P1030860.JPG') }}" class="d-block w-100"
+                         alt="Omgeving HauseMoltall">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5><a>Omgeving</a></h5>
+                        <p>Geniet van al het moois wat Karinthië te bieden heeft</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/img/HuisOmgeving/wandelen/DSC05252.JPG') }}" class="d-block w-100"
+                         alt="Activiteiten HauseMoltall">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Activiteiten</h5>
+                        <p>Genoeg te beleven in de buurt!</p>
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="{{ asset('assets/img/HuisOmgeving/zomer/P1030860.JPG') }}" class="d-block w-100" alt="Omgeving HauseMoltall">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5><a>Omgeving</a></h5>
-                    <p>Geniet van al het moois wat Karinthië te bieden heeft</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('assets/img/HuisOmgeving/wandelen/DSC05252.JPG') }}" class="d-block w-100" alt="Activiteiten HauseMoltall">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Activiteiten</h5>
-                    <p>Genoeg te beleven in de buurt!</p>
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Vorige</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Volgende</span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Vorige</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Volgende</span>
-        </a>
     </div>
-</div>
-    
+
 @endsection
