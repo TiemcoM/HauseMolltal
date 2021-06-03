a6a0f47a762ba81454c5ae6fa73df53048025dc2
Twan <97069905@st.deltion.nl>
2021-06-03 15:24:46 +0200

    tijdelijk
    tijdelijk iets

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,6 @@
-<footer class="footer">
+<footer class="footer" style="position: relative">
     <div class="container">
         <span class="text-muted">&copy; {{date ('Y')}} Hause Molltal - All rights reserved </span>
     </div>
 </footer>
+
