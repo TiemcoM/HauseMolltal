1dd826d049a05c3f5b6970e301ec2785a8eb75f8
ImDamian <77979290+ImDamian@users.noreply.github.com>
2021-06-03 12:59:32 +0200

    fixed slider
    slider gefixt

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,35 +5,6 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-<div class="bd-example">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{ asset('assets/img/HuisOmgeving/huis/huis2.JPG') }}" class="d-block w-100" alt="HauseMoltall">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Hause Moltall</h5>
-                    <p>Kom tot rust in de Oostenrijkse natuur!</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('assets/img/HuisOmgeving/zomer/zomer1.JPG') }}" class="d-block w-100" alt="Omgeving HauseMoltall">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5><a>Omgeving</a></h5>
-                    <p>Geniet van al het moois wat Karinthië te bieden heeft</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('assets/img/HuisOmgeving/wandelen/wandelen6.JPG') }}" class="d-block w-100" alt="Activiteiten HauseMoltall">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Activiteiten</h5>
-                    <p>Genoeg te beleven in de buurt!</p>
-=======
     <div class="bd-example">
         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
@@ -43,7 +14,7 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ asset('assets/img/HuisOmgeving/huis/P1030790.JPG') }}" class="d-block w-100"
+                    <img src="{{ asset('assets/img/HuisOmgeving/huis/huis2.JPG') }}" class="d-block w-100"
                          alt="HauseMoltall">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Hause Moltall</h5>
@@ -51,7 +22,7 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('assets/img/HuisOmgeving/zomer/P1030860.JPG') }}" class="d-block w-100"
+                    <img src="{{ asset('assets/img/HuisOmgeving/zomer/zomer1.JPG') }}" class="d-block w-100"
                          alt="Omgeving HauseMoltall">
                     <div class="carousel-caption d-none d-md-block">
                         <h5><a>Omgeving</a></h5>
@@ -59,28 +30,14 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('assets/img/HuisOmgeving/wandelen/DSC05252.JPG') }}" class="d-block w-100"
+                    <img src="{{ asset('assets/img/HuisOmgeving/wandelen/wandelen6.JPG') }}" class="d-block w-100"
                          alt="Activiteiten HauseMoltall">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Activiteiten</h5>
                         <p>Genoeg te beleven in de buurt!</p>
+
+
                     </div>
->>>>>>> 8654aac8e7f12ca4724e995c59a4adce9746d407
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Vorige</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Volgende</span>
-            </a>
-        </div>
-    </div>
-<<<<<<< HEAD
-</div>
-=======
->>>>>>> 8654aac8e7f12ca4724e995c59a4adce9746d407
+
 
 @endsection
