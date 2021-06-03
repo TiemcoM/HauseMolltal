4e992354b6d4be4348013981e3c8e3a2403c0a77
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-06-03 13:53:27 +0200

    Added reservation model and migration

---
+++ app/Models/Reservations.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservations extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'customer', 'e-mail', 'phonenumber', 'room', 'start', 'end'
+    ];
+}
---
+++ database/migrations/2021_06_03_114724_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('customer');
+            $table->string('e-mail');
+            $table->integer('phonenumber');
+            $table->integer('room');
+            $table->date('start');
+            $table->date('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
