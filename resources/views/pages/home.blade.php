e11c78f071c6d13e6d40febcad787fd140613fd3
Twan <97069905@st.deltion.nl>
2021-06-03 14:55:37 +0200

    Home-carousel indicators
    Added indicators in carousel on home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -35,9 +35,19 @@
                     <div class="carousel-caption d-none d-md-block" style="background-color:rgba(0,0,0,0.4);">
                         <h5>Activiteiten</h5>
                         <p>Genoeg te beleven in de buurt!</p>
-
-
                     </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
 
 
 @endsection
