3fe9c21d4b3b094e58fb688e3ad41f2c417f23eb
Twan <97069905@st.deltion.nl>
2021-06-11 10:52:33 +0200

    Footer at bottom
    Footer zit nu altijd onderop en overlapt niet meer met de content

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,3 @@
-<footer class="footer" style="position: relative">
-    <div class="container">
-        <span class="text-muted">&copy; {{date ('Y')}} Hause Molltal - All rights reserved </span>
-    </div>
-</footer>
-
+<div class="container" style="text-align: center; padding: 5px 0 5px 0">
+    <span class="text-muted">&copy; {{date ('Y')}} Hause Molltal - All rights reserved </span>
+</div>
