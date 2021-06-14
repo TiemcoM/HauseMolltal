d6cea4e4b5d8d5419789e3e1603006be01a85eca
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-06-14 15:40:44 +0200

    Created kalender page
    Current fix: Iframe of calendar. Need to work on vue in blade

---
+++ resources/views/pages/calendar.blade.php
@@ -0,0 +1,9 @@
+@section('title')
+    Kalender
+@endsection
+
+@extends('layouts.app')
+
+@section('content')
+    <iframe src="http://127.0.0.1:8000/admin/calendar" title="Hause Molltal Kalender" width="100%" height="800" scrolling="no"></iframe>
+@endsection
