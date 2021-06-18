686e91ffef17de77b7eec6d6bef1a33edb61e919
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-06-18 12:37:11 +0200

    Created new pages

---
+++ resources/js/components/EditText.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "EditText"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/TextList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "TextList"
+}
+</script>
+
+<style scoped>
+
+</style>
